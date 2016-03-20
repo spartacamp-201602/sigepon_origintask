4699fc2e0b1b890799c598f78795ad0b24ec26c1
Kikuchi Shigeo <K@sigepon.local>
2016-03-20 10:08:00 +0900

    index()アクション実装 Taskモデル作成(中身はから)

--- app/Controller/TasksController.php
+++ app/Controller/TasksController.php
@@ -1,5 +1,19 @@
 <?php
 
 class TasksController extends AppController {
-    public $scaffold;
+    //public $scaffold;
+	
+	public $helper = array('Html');
+	
+	public function index(){
+		$options = array(
+			'conditions' => array(
+			'Task.status' => 0)
+	);
+	
+	$tasks = $this->Task->find('all', $options);
+	
+	$this->set('tasks', $tasks);
+	
+	}
 }
